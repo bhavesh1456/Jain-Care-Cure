bb29ce04374cfd73caaa132cf40bbb213169d83b
Bhavesh Thale <21256563+bhavesh1456@users.noreply.github.com>
2021-12-09 12:44:05 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,38 @@
+<?php include 'header.php';?>
+
+
+<!-- Content -->
+<div id="content">
+
+    <!-- Intro -->
+    <section class="p-t-b-150">
+        <div class="container">
+            <div class="intro-main">
+                <div class="row">
+
+                    <!-- Intro Detail -->
+                    <div class="col-md-7">
+                        <div class="text-sec padding-right-0">
+                            <h5>Health Check Up At Care And Cure</h5>
+                            
+                            <ul class="row">
+                                
+                            </ul>
+                        </div>
+                    </div>
+
+                    <!-- Intro Timing -->
+                    <div class="col-md-5"> <img class="img-responsive intro-img" src="images/intro-img.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+</div>
+
+<!-- Footer -->
+<!--======= FOOTER =========-->
+
+<?php include 'footer.php';?>
---
+++ index.php
@@ -0,0 +1,275 @@
+
+  <?php include 'header.php';?>
+  <!-- Bnr Header -->
+  <section class="main-banner"><br>
+    <br><br><br><br><br><br><br><br><br><h1 style="color:#33bbff;"><b><Center>Jain Care And Cure</Center></b></h1><br><br><br><br><br><br><br><br><br>
+  </section>
+  
+  <!-- Content -->
+  <div id="content"> 
+    
+    <!-- Intro -->
+    <section class="p-t-b-150">
+      <div class="container">
+        <div class="intro-main">
+          <div class="row"> 
+            
+            <!-- Intro Detail -->
+            <div class="col-md-8">
+              <div class="text-sec">
+                <h5>Health Check Up At Jain Care And Cure</h5>
+                <p>Experience the best Doctors, Nurses and other staff where you feel like family and home. Best treatment best analysis and best results Thanks to Dr. Sanjeev Jain.</p>
+                <ul class="row">
+                  <li class="col-sm-6">
+                    <h6> <i class="lnr  lnr-checkmark-circle"></i> EMERGENCY CASE</h6>
+                    <p></p>
+                  </li>
+                  
+                  <li class="col-sm-6">
+                    <h6> <i class="lnr  lnr-checkmark-circle"></i> ONLINE APPOINTMENT</h6>
+                    <p></p>
+                  </li>
+                  <li class="col-sm-6">
+                    <h6> <i class="lnr  lnr-checkmark-circle"></i> FREE MEDICAL COUNSELING</h6>
+                    <p> </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            
+            <!-- Intro Timing -->
+            <div class="col-md-4">
+              <div class="timing"> <i class="lnr lnr-clock"></i>
+                <ul>
+                  <li> Monday <span>10.00 AM - 12.00 PM </span></li>
+                  <li> Tuesday <span>8.00 AM - 4.00 PM</span></li>
+                  <li> Wednesday <span>8.00 AM - 4.00 PM</span></li>
+                  <li> Thursday <span>8.00 AM - 4.00 PM</span></li>
+                  <li> Friday <span>8.00 AM - 4.00 PM</span></li>
+                  <li> Saturday <span>8.00 AM - 4.00 PM</span></li>
+                  <li> Sunday <span>8.00 AM - 4.00 PM</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+  
+
+    
+    <!-- DOCTOR LIST -->
+    
+    
+    
+    <!-- Departments -->
+    <section class="p-t-b-150 padding-bottom-0">
+      <div class="container"> 
+        
+        <!-- Heading -->
+      
+      <!-- Department -->
+      <div class="department">
+        <div class="container"> 
+          <!-- Tab Panel -->
+          <div role="tabpanel">
+            <div class="dep-sec-nav"> 
+              <!-- Nav tabs -->
+              <ul class="nav nav-tabs" role="tablist">
+                
+                <li role="presentation" class="active"><a href="#cardiology" role="tab" data-toggle="tab">Cardiology </a></li>
+                
+              </ul>
+            </div>
+            
+            <!-- Tab Content -->
+            <div class="tab-content"> 
+              
+              <!-- Dental Depatment -->
+              <div role="tabpanel" class="tab-pane fade" id="dental"> 
+                <!-- Depatment Text Section -->
+                <div class="dep-sec-txt">
+                  <div class="row">
+                    
+                    
+                    <!-- Doctor Img -->
+                    <div class="col-md-offset-1 col-md-4">
+                      <div class="doctor-img"> <img class="img-responsive" src="User.png" alt="" >
+                        <div class="name-doc">
+                          <h6>Dr. <span>Cardiology Specialists</span></h6>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
+              <!-- Cardiology Depatment -->
+              <div role="tabpanel" class="tab-pane fade in active" id="cardiology"> 
+                <!-- Depatment Text Section -->
+                <div class="dep-sec-txt">
+                  <div class="row">
+                    <div class="col-md-7">
+                      <h5>Our Best <span>CARDIOLOGY</span> Services</h5>
+                      <p> <br>
+                        <br>
+                        </p>
+                      
+                      <!-- List Style Check -->
+                      <ul class="list-style-check">
+                      
+                        <li> Feel like you are at Home Services </li>
+                      
+                        <li> Save your Money and Time with us </li>
+                       
+                        <li> Blood Bank </li>
+                      </ul>
+                    </div>
+                    
+                    <!-- Doctor Img -->
+                    <div class="col-md-offset-1 col-md-4">
+                      <div class="doctor-img"> <img class="img-responsive" src="User.png" alt="" >
+                        <div class="name-doc">
+                          <h7><b>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp   
+						  Dr. Sanjeev Jain 
+						  <br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+						  Cardiology Specialists</b></h7>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
+              <!-- For disabled 
+              <div role="tabpanel" class="tab-pane fade" id="for-disabled"> 
+                <!-- Depatment Text Section 
+                <div class="dep-sec-txt">
+                  <div class="row">
+                    <div class="col-md-7">
+                      <h5>Our Best Services <span>For Disabled</span> Persons </h5>
+                        <br>
+                      
+                      <!-- List Style Check 
+                      <ul class="list-style-check">
+                        <li> Qualified Staff of Doctors </li>
+                        <li> Feel like you are at Home Services </li>
+                        <li> 24x7 Emergency Services </li>
+                        <li> Save your Money and Time with us </li>
+                        <li> Medicine Research </li>
+                        <li> Blood Bank </li>
+                      </ul>
+                    </div>
+                    
+                    <!-- Doctor Img
+                    <div class="col-md-offset-1 col-md-4">
+                      <div class="doctor-img"> <img class="img-responsive" src="" alt="" >
+                        <div class="name-doc">
+                          <h6> Dr. <span>Cardiology Specialists</span></h6>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
+              <!-- Ophthalmology 
+              <div role="tabpanel" class="tab-pane fade" id="ophthalmology"> 
+                <!-- Depatment Text Section 
+                <div class="dep-sec-txt">
+                  <div class="row">
+                    <div class="col-md-7">
+                      <h5>Our Best <span>Ophthalmology</span> Services</h5>
+                        <br>
+                      
+                      <!-- List Style Check 
+                      <ul class="list-style-check">
+                        <li> Qualified Staff of Doctors </li>
+                        <li> Feel like you are at Home Services </li>
+                        <li> 24x7 Emergency Services </li>
+                        <li> Save your Money and Time with us </li>
+                        <li> Medicine Research </li>
+                        <li> Blood Bank </li>
+                      </ul>
+                    </div>
+                    
+                    <!-- Doctor Img 
+                    <div class="col-md-offset-1 col-md-4">
+                      <div class="doctor-img"> <img class="img-responsive" src="" alt="" >
+                        <div class="name-doc">
+                          <h6> <span>Ophthalmology Specialists</span></h6>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
+              <!-- emergency 
+              <div role="tabpanel" class="tab-pane fade" id="emergency"> 
+                <!-- Depatment Text Section 
+                <div class="dep-sec-txt">
+                  <div class="row">
+                    <div class="col-md-7">
+                      <h5>Our Best <span>Emergency</span> Services</h5>
+                        <br>
+                      
+                      <!-- List Style Check 
+                      <ul class="list-style-check">
+                        <li> Qualified Staff of Doctors </li>
+                        <li> Feel like you are at Home Services </li>
+                        <li> 24x7 Emergency Services </li>
+                        <li> Save your Money and Time with us </li>
+                        <li> Medicine Research </li>
+                        <li> Blood Bank </li>
+                      </ul>
+                    </div>
+                    
+                    <!-- Doctor Img 
+                    <div class="col-md-offset-1 col-md-4">
+                      <div class="doctor-img"> <img class="img-responsive" src="" alt="" >
+                        <div class="name-doc">
+                          <h6> <span>Emergency Specialists</span></h6>
+                        </div>
+                      </div>
+                    </div>-->
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+	 <section class="p-t-b-150">
+      <div class="container"> 
+        
+        <!-- Heading -->
+        <div>
+          <h4><b>Contact        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+								&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp		  Address</b></h4>   
+		  <h6>9527459656   &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+								&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp							
+                          Dr. Sanjeev Jain, Bamangaon, Gothi,</h6>
+          <h6>02141-220685 &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+								&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+   Alibag Maharashtra</h6>
+          <h6>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp  India</h6>
+          </div>
+		  
+        
+        <!-- Services -->
+        
+      </div>
+    </section>
+    
+   
+    
+    
+  </div>
+  
+  <!-- Footer -->
+<?php include 'footer.php';?>
